917cb8cf7498c4a595f87a88eafe9bddd11c37d7
leo <leno3z4rrrri@gmail.com>
2026-09-10 22:13:46 +0100

    Integrate Circle features into core wallet surfaces

--- app/circle/page.tsx
+++
@@ -1,7 +0,0 @@
-import { AuthGate } from '@/components/auth-gate';
-import { AppShell } from '@/components/app-shell';
-import { CircleCenter } from '@/components/circle-center';
-
-export default function CirclePage() {
-  return <AuthGate><AppShell title="Circle"><CircleCenter /></AppShell></AuthGate>;
-}
